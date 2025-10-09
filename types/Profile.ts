1df8ac72d4e01c96052be90e6b8d848ae9b24ae8
unknown <04albfra@skola.boras.se>
2025-10-09 17:53:31 +0200

    Fixat Profile

---
+++ types/Profile.ts
@@ -0,0 +1,9 @@
+export type Profile = {
+    id: number;
+    HouseHoldID: number;
+    Name: string;
+    isOwner: boolean;
+    AvatarID: string;
+    AccountId: number;
+    isRequest: boolean;
+}
