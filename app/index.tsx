1dfcd021ec8c7a099219266b1fd3bdb94e5c2bda
nisa888 <88niksar@skola.boras.se>
2025-10-16 22:05:54 +0200

    index fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,1 +1,19 @@
+import {View, Text, Button} from 'react-native'
+import {Link} from 'expo-router'
 
+export default function HomeScreen() {
+   return (
+      <View>
+         <Text>Test</Text>
+         <Link href='/login' asChild>
+            <Button title='Gå till Login' />
+         </Link>
+         <Link href='/createUser' asChild>
+            <Button title='Skapa användare' />
+         </Link>
+         <Link href='/househould' asChild>
+            <Button title='hushåll' />
+         </Link>
+      </View>
+   )
+}
