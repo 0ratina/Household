470246ae722af2f3897a769d8defd06a4173a04d
Karl Pulli <kalle.pulli@hotmail.com>
2025-10-23 23:14:05 +0200

    Added app.config.js for Firebase Config

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,15 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
+import Constants from 'expo-constants';
 
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-};
+const firebaseConfig = Constants.expoConfig?.extra?.firebase;
 
 
 const app = initializeApp(firebaseConfig);
@@ -21,4 +15,3 @@ export const auth = initializeAuth(app, {
 });
 
 export const db = getFirestore(app);
-
