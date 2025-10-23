8b0fca2c4e729a6d5712c064a9193cede0d05e6e
Karl Pulli <kalle.pulli@hotmail.com>
2025-10-23 22:35:12 +0200

    Config fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,14 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDgLjCZP2vt_IrdMnoIJH3MJaqOm9shI1k",
-    authDomain: "AIzaSyDgLjCZP2vt_IrdMnoIJH3MJaqOm9shI1k",
-    projectId: "household-app-40c0a",
-    storageBucket: "household-app-40c0a.firebasestorage.app",
-    messagingSenderId: "448686221081",
-    appId: "1:448686221081:web:2ece0214e101bb8c838788"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 
@@ -20,3 +21,4 @@ export const auth = initializeAuth(app, {
 });
 
 export const db = getFirestore(app);
+
