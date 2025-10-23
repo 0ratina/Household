09b9815eb4dba51332358cee9b7cd778b9068330
Karl Pulli <kalle.pulli@hotmail.com>
2025-10-23 22:07:36 +0200

    Cleanup + Routing fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: "AIzaSyDgLjCZP2vt_IrdMnoIJH3MJaqOm9shI1k",
+    authDomain: "AIzaSyDgLjCZP2vt_IrdMnoIJH3MJaqOm9shI1k",
+    projectId: "household-app-40c0a",
+    storageBucket: "household-app-40c0a.firebasestorage.app",
+    messagingSenderId: "448686221081",
+    appId: "1:448686221081:web:2ece0214e101bb8c838788"
 };
 
 const app = initializeApp(firebaseConfig);
