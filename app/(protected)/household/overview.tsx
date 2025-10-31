4b2755d05cc6993706a62ee0c7a2b14ccac1be46
Karl Pulli <kalle.pulli@hotmail.com>
2025-10-31 15:49:31 +0100

    Add button to create task

--- app/(protected)/household/overview.tsx
+++ app/(protected)/household/overview.tsx
@@ -1,48 +1,37 @@
-import { View, Text, StyleSheet} from 'react-native';
-import {Button} from '@react-navigation/elements';
-import {Link} from 'expo-router';
+import { router } from "expo-router";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
 
-export default function HouseHoldOverview() {
+export default function TasksScreen() {
   return (
-    <View>
-      <Link style={styles.household}href='/househould'>
-      <Text >Hushåll 1</Text>
-               </Link>
-               <Link style={styles.household}href='/househould'>
-      <Text >Hushåll 2</Text>
-               </Link>
-               <Link style={styles.household}href='/househould'>
-      <Text >Hushåll 3</Text>
-               </Link>
-               <Link style={styles.household}href='/househould'>
-      <Text >Hushåll 4</Text>
-               </Link>
-               <Link style={styles.household}href='/househould'>
-      <Text >Hushåll 5</Text>
-               </Link>
-               <Link style={styles.household}href='/househould'>
-      <Text >Hushåll 6</Text>
-               </Link>
+    <View style={{ flex: 1 }}>
+
+
+
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => router.push("/createtask")}
+      >
+        <Text style={styles.addButtonText}>+ Lägg till</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
-  household: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    padding: 25,
-    color: '#222',
-    backgroundColor: "#FFFFFF",
-    borderRadius: 14,
-    marginBottom: 14,
-    shadowColor: "#000",
-    shadowOpacity: 0.08,
-    shadowRadius: 8,
-    shadowOffset: { width: 0, height: 4 },
-    elevation: 3,
+  addButton: {
+    backgroundColor: "#111",
+    paddingVertical: 14,
+    borderRadius: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 16,
+    marginTop: 16,
+    marginBottom: 8,
   },
-  
+  addButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
   }
-  )
+});
