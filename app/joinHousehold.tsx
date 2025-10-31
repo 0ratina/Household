9b3936e24302b904fa00e913e56e5607cf9ee836
Vedin <vedin22@gmail.com>
2025-10-31 18:53:11 +0100

    created handlJoin function to verify code and output alert

--- app/joinHousehold.tsx
+++ app/joinHousehold.tsx
@@ -1,6 +1,25 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useState } from "react";
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { verifyHouseholdCode } from '../src/api/joinHousehold';
 
 export default function joinHousehold () {
+
+    const [code, setCode] = useState("");
+
+    const handleJoin = async () => {
+        if (!code.trim()) {
+            Alert.alert("Skriv in en kod!")
+            return;
+        }
+        const household = await verifyHouseholdCode(Number(code.trim()));
+
+        if (household) {
+            Alert.alert(`Du gick med i: ${household.Name}`)
+        }
+        else {
+            Alert.alert("Koden matchar inget hushåll")
+        }
+    }
     return (
 
         <View style={styles.container}>
@@ -8,15 +27,15 @@ export default function joinHousehold () {
             <View style = {styles.inputView}>
                 <TextInput style = {styles.input}
                     placeholder = "Kod"
+                    value={code}
+                    onChangeText={setCode}
                 />
             </View>
 
-
-            <TouchableOpacity style= {styles.joinButton}> 
+            <TouchableOpacity style= {styles.joinButton} onPress={handleJoin}> 
                 <Text style = {{fontSize: 18}}>Gå med</Text>
             </TouchableOpacity>
         </View>
-
 
     )
 }
