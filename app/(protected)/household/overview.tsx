b8eb679c5a51e6686e61d1d8bdd09f78c0b720d7
Karl Pulli <kalle.pulli@hotmail.com>
2025-10-31 16:35:13 +0100

    added query

--- app/(protected)/household/overview.tsx
+++ app/(protected)/household/overview.tsx
@@ -1,7 +1,8 @@
 import { router } from "expo-router";
 import { collection, getDocs } from "firebase/firestore";
 import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
-import { db } from "../../../src/firebase";
+import { auth, db } from "../../../src/firebase";
+import { useQuery } from "@tanstack/react-query";
 
 async function getTasksForHousehold(householdId: string) {
   const tasksRef = collection(db, `households/${householdId}/tasks`);
@@ -14,24 +15,36 @@ async function getTasksForHousehold(householdId: string) {
   }));
 }
 
-export default function TasksScreen() {
+export default function OverviewScreen() {
+  const uid = auth.currentUser?.uid;
+
+  const { data: tasks = [], isLoading } = useQuery({
+    queryKey: ["allTasks"],
+    enabled: !!uid,
+    queryFn: () => getAllMyHouseholdTasks(uid!),
+  });
+
+
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
+      <View style={{ flex: 1 }}>
 
 
 
-      <TouchableOpacity
-        style={styles.addButton}
-        onPress={() => router.push("/createtask")}
-      >
-        <Text style={styles.addButtonText}>+ Lägg till</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => router.push("/createtask")}
+        >
+          <Text style={styles.addButtonText}>+ Lägg till</Text>
+        </TouchableOpacity>
 
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#EFEFEF" },
   addButton: {
     backgroundColor: "#111",
     paddingVertical: 14,
@@ -48,3 +61,7 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   }
 });
+
+function getAllMyHouseholdTasks(arg0: string): any {
+  throw new Error("Function not implemented.");
+}
