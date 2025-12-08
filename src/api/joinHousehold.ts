c505bbe86ea5c70df100b3508e196cb7c958a125
Vedin <vedin22@gmail.com>
2025-12-08 04:39:59 +0100

    Fixed linkUserToHousehold and added isUserInHousehold check and updated joinHousehold

--- src/api/joinHousehold.ts
+++ src/api/joinHousehold.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs, query, where, doc, updateDoc,setDoc} from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, query, setDoc, where } from "firebase/firestore";
 import { db } from "../firebase";
 
 export async function verifyHouseholdCode(code: number) {
@@ -17,5 +17,39 @@ export async function verifyHouseholdCode(code: number) {
 
 export async function linkUserToHousehold(accountId: string, householdId: string) {
   const profileRef = doc(db, "profiles", accountId);
-  await setDoc(profileRef, { HouseHoldID: householdId }, { merge: true });
+  const profileSnap = await getDoc(profileRef);
+
+  let households: string[] = [];
+  if (profileSnap.exists()) {
+    const profileData = profileSnap.data();
+
+    if (Array.isArray(profileData?.HouseHoldID)) {
+      households = (profileData.HouseHoldID);
+    }
+    else if (profileData?.HouseHoldID) {
+      households = [profileData.HouseHoldID];
+    }
+  }
+
+  if (!households.includes(householdId)) {
+    households.push(householdId);
+  }
+
+  await setDoc(profileRef, { HouseHoldID: households }, { merge: true });
+}
+
+export async function isUserInHousehold(accountId: string, householdId: string) {
+    const profileRef = doc(db, "profiles", accountId);
+    const profileSnap = await getDoc(profileRef);
+
+    if (!profileSnap.exists()) return false;
+
+    const profileData = profileSnap.data();
+    const households: string[] = Array.isArray(profileData?.HouseHoldID)
+    ? profileData.HouseHoldID
+    : profileData?.HouseHoldID
+    ? [profileData.HouseHoldID]
+    : [];
+
+    return households.includes(householdId);
 }
